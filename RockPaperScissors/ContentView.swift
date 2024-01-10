d75b87c0d84f0bc36149149e23fbee0f0a65bed5
KevinMuniz1 <munizkevin@outlook.com>
2024-01-09 20:24:04 -0500

    The game is finished with better functionality, the user can now view their score at the end. An alert tells them when it is game over

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,36 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    private var userQuestion = ["Which option will win?", "Which option will lose?"]
-    private var handOptions = ["🖐️", "✊", "✌️"]
-    @State private var computerHandOptions = ["🖐️", "✊", "✌️"]
-    @State private var randomQuestion = Int.random(in: 0...1)
-    @State private var userAnswer = ""
+    private var handOptions = ["✊", "🖐️", "✌️"]
+    @State private var randomMove = Int.random(in: 0...2)
+    @State private var shouldWin = Bool.random()
     @State private var winOrLoseAlert = false
     @State private var gameover = false
     @State private var score = 0
     @State private var totalRounds = 0
     @State private var alertTitle = ""
-    @State private var messageTitle = ""
     var body: some View {
         NavigationStack {
             VStack{
                 Text("The computer has chosen")
                     .font(.headline)
-                Text(computerHandOptions[0])
+                Text(handOptions[randomMove])
                     .font(.system(size: 150))
                     .padding(.bottom, 50)
-                Text(userQuestion[Int.random(in: 0...1)])
-                    .font(.headline)
+                if shouldWin {
+                    Text("Which option will win?")
+                        .foregroundStyle(.green)
+                        .font(.headline)
+                } else {
+                    Text("Which option will lose?")
+                        .foregroundStyle(.red)
+                        .font(.headline)
+                }
                 HStack(){
-                    ForEach(handOptions, id: \.self){ number in
-                        Button(number) {
-                            computerHandOptions.shuffle()
-                            userAnswer = number
-                            checkAnswer()
+                    ForEach(0..<3){ number in
+                        Button(handOptions[number]) {
+                            checkAnswer(choice: number)
                         }
                         .font(.system(size: 75))
-                            .padding()
+                        .padding()
                     }
                 }
                 Text("Score: \(score)")
@@ -46,34 +48,47 @@ struct ContentView: View {
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(.regularMaterial)
             .background(.blue)
-                .navigationTitle("Rock Paper Scissors")
+            .navigationTitle("Rock Paper Scissors")
         }.alert("Game Over", isPresented: $gameover){
             Button("Start Over"){
-                restartGame()
+                resetGame()
             }
         } message: {
             Text("Your final score is \(score)")
         }
-        .alert(alertTitle, isPresented: $winOrLoseAlert){
-            Button("Try Again!"){
-                nextQuestion()
+    }
+    func checkAnswer(choice: Int){
+        let winningMoves = [1, 2, 0]
+        let didWin: Bool
+        
+        if shouldWin {
+            didWin = choice == winningMoves[randomMove]
+        } else {
+            didWin = winningMoves[choice] == randomMove
+        }
+        
+        if didWin {
+            score += 1
+        } else {
+            if score > 0 {
+                score -= 1
             }
-        } message: {
-            Text(messageTitle)
+        }
+        if totalRounds == 10 {
+            gameover = true
+        } else {
+            randomMove = Int.random(in: 0...2)
+            shouldWin.toggle()
+            totalRounds += 1
         }
     }
-    func checkAnswer(){
-        
-    }
     
-    func nextQuestion() {
-        
-    }
-    func restartGame() {
+    func resetGame() {
+        randomMove = Int.random(in: 0...2)
+        shouldWin = Bool.random()
         score = 0
         totalRounds = 0
     }
-    
 }
 
 #Preview {
